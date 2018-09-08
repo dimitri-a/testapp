a8b5170039e969e7d9867bffdb3c65e9de888abd
Dimitri C <couwenberg@gmail.com>
2018-09-08 15:22:31 +1000

    update

--- src/App.js
+++ src/App.js
@@ -2,16 +2,44 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Widget from './Widget'
+import axios from 'axios';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { icon: '', degrees: 0, title: '', lat: 0, lon: 0 };
+  }
+
+  componentDidMount() {
+
+    const APPKEY = 'c7b5b62a01a84a2d274930a57e180950';
+    let url = 'https://samples.openweathermap.org/data/2.5/weather';
+
+
+    axios("https://api.ipdata.co/?api-key=test", function (response) {
+
+      this.setState({ lon: response.long, lat: response.lat });
+
+      axios(url + '?lat=' + this.state.lon + '&lat=' + this.state.lat + '&' + 'appid=' + APPKEY).then((data) => {
+        this.setState({
+          icon: data.icon,
+          degrees: data.degrees,
+
+        })
+      });
+    });
+
+
+
+
+  }
+
   render() {
-    let Title='Sydney weather';
-    let icon='http://openweathermap.org/img/w/10d.png'
-    let degrees='26';
     return (
       <div className="App">
-      <Widget title={Title} className ="imaag" icon={icon} degrees={degrees}></Widget>
-       
+        <Widget title={this.state.title} className="imaag" icon={this.state.icon} degrees={this.state.degrees}></Widget>
+
       </div>
     );
   }
--- src/Widget.js
+++ src/Widget.js
@@ -1,7 +1,6 @@
 import React from 'react'
 
 export default ({ title, icon, degrees }) => {
-    debugger
     return (
         <div>
             {title}
